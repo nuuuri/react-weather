fe37f45c28916f00c7d77e6d8a92c77d07a07598
nuuuri <sbfl125@gmail.com>
2025-01-10 20:05:04 +0900

    [feat] tanstack query 설치

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import '@/styles/global.css';
+
 import App from './App';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={false} />
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </QueryClientProvider>
 );
